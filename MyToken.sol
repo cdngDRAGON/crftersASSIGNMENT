755ab34dfcc69a9772c35fabe871d4024c688dd4
cdngDRAGON <142192490+cdngDRAGON@users.noreply.github.com>
2024-06-03 17:53:50 +0530

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    // Public variables
+    string public name = "VishwaGuru";         // Token Name
+    string public symbol = "DRGN";           // Token Abbreviation
+    uint256 public totalSupply;             // Total Supply
+
+    // Mapping from address to balance
+    mapping(address => uint256) public balances;
+
+    // Mint function
+    function mint(address _to, uint256 _value) public {
+        totalSupply += _value;
+        balances[_to] += _value;
+    }
+
+    // Burn function
+    function burn(address _from, uint256 _value) public {
+        require(balances[_from] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balances[_from] -= _value;
+    }
+}
